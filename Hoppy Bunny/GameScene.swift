6e9438034b40a051cf09a56386bf1eb896d79cfb
Jessica Trinh <jtjessicatrinh@gmail.com>
2019-08-01 18:36:15 -0700

    Implement motion/rotation to bunny

--- Hoppy Bunny/GameScene.swift
+++ Hoppy Bunny/GameScene.swift
@@ -10,6 +10,8 @@ import SpriteKit
 class GameScene: SKScene {
     
     var hero: SKSpriteNode!
+    var sinceTouch : CFTimeInterval = 0
+    let fixedDelta: CFTimeInterval = 1.0 / 60.0 /* 60 FPS */
     
     override func didMove(to view: SKView) {
         /* Setup your scene here */
@@ -23,6 +25,13 @@ class GameScene: SKScene {
         
         /* Apply vertical impulse */
         hero.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 300))
+        
+        /* Apply subtle rotation */
+        hero.physicsBody?.applyAngularImpulse(1)
+        
+        /* Reset touch timer */
+        sinceTouch = 0
+        
     }
     
     override func update(_ currentTime: TimeInterval) {
@@ -35,6 +44,18 @@ class GameScene: SKScene {
         if velocityY > 400 {
             hero.physicsBody?.velocity.dy = 400
         }
+        /* Apply falling rotation */
+        if sinceTouch > 0.2 {
+            let impulse = -20000 * fixedDelta
+            hero.physicsBody?.applyAngularImpulse(CGFloat(impulse))
+        }
+        
+        /* Clamp rotation */
+        hero.zRotation.clamp(v1: CGFloat(-90).degreesToRadians(), CGFloat(30).degreesToRadians())
+        hero.physicsBody?.angularVelocity.clamp(v1: -1, 3)
+        
+        /* Update last touch timer */
+        sinceTouch += fixedDelta
     }
     
     
