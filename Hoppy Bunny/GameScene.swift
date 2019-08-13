7ee6f26b9d7eb1a4211b12491a9f6c3a97294dfa
Jessica Trinh <jtjessicatrinh@gmail.com>
2019-08-13 13:44:40 -0700

    implement obstacle scrolling

--- Hoppy Bunny/GameScene.swift
+++ Hoppy Bunny/GameScene.swift
@@ -73,6 +73,9 @@ class GameScene: SKScene {
         
         /* Process world scrolling */
         scrollWorld()
+        
+        /* Process obstacles */
+        updateObstacles()
     }
     
     func scrollWorld() {
@@ -95,6 +98,29 @@ class GameScene: SKScene {
                 ground.position = self.convert(newPosition, to: scrollLayer)
             }
         }
+    }
+    
+    func updateObstacles() {
+        /* Update Obstacles */
+        
+        obstacleLayer.position.x -= scrollSpeed * CGFloat(fixedDelta)
+        
+        /* Loop through obstacle layer nodes */
+        for obstacle in obstacleLayer.children as! [SKReferenceNode] {
+            
+            /* Get obstacle node position, convert node position to scene space */
+            let obstaclePosition = obstacleLayer.convert(obstacle.position, to: self)
+            
+            /* Check if obstacle has left the scene */
+            if obstaclePosition.x <= -26 {
+                // 26 is one half the width of an obstacle
+                
+                /* Remove obstacle node from obstacle layer */
+                obstacle.removeFromParent()
+            }
+            
+        }
+        
     }
     
     
